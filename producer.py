896299d948e734e768d0a8a1907a8f666192bb3a
Benjamin Ikeh <b.ikeh@yahoo.com>
2021-08-18 17:31:36 +0000

    send message to a single partition

--- producer.py
+++ producer.py
@@ -10,8 +10,12 @@ def json_serializer(data):
     return json.dumps(data).encode('utf-8')
 
 
+def get_partition(key, all, available):
+    return 0
+
+
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                         value_serializer=json_serializer)
+                         value_serializer=json_serializer, partitioner=get_partition)
 
 if __name__ == '__main__':
     while 1 == 1:
